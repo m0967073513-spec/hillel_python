131f273eb44f6e277231b2ffee43c89fd2b9178f
Maksicos <maksim.yarovyi@gmail.com>
2026-01-05 00:58:49 +0100

    Implement list element repositioning
    This script moves the last element of multiple lists to the front and prints the modified lists.

---
+++ home_work.3.2.py
@@ -0,0 +1,16 @@
+# ДЗ 3.2. Перемістити елемент у списку
+
+lst = [1, 2, 3, 4, 5]
+last = lst.pop()
+lst.insert(0, last)
+print(lst)
+
+lst = [6,7,8,9,10]
+last = lst.pop() 
+lst.insert(0, last)
+print(lst)
+
+lst = [11,12,13,14,15]
+last = lst.pop()
+lst.insert(0, last)
+print(lst)
