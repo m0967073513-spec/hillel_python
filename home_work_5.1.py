2fbd707a0e3941ccc293497e5665427d72f1ebe6
Maksicos <m0967073513@gmail.com>
2026-01-17 23:25:34 +0100

    Add files via upload

---
+++ home_work_5.1.py
@@ -0,0 +1,38 @@
+# ДЗ 5.1. Ім'я змінної
+
+import string
+import keyword
+
+while True:
+
+    name = input("Введіть коректне ім'я змінної: ")
+    bad_punct = string.punctuation.replace("_", "")
+    is_valid = True
+
+    if name == "":
+        is_valid = False
+
+    elif name[0].isdigit():
+        is_valid = False
+
+    elif any(ch.isupper() for ch in name):
+        is_valid = False
+
+    elif any(ch.isspace() or ch in bad_punct for ch in name):
+        is_valid = False
+
+    elif "__" in name:
+        is_valid = False
+
+    elif name in keyword.kwlist:
+        is_valid = False
+
+    print(is_valid)
+
+    choice = input("Бажаєте продовжити? (yes / y — так, no — ні): ").lower()
+    if choice not in ("yes", "y"):
+        print("Роботу завершено")
+        break
+
+# Обгорнув у цикл while для зручності тестування
+    
