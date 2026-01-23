51349eee047bab7ad5d58d2d41c033dbc57215ad
Maksicos <m0967073513@gmail.com>
2026-01-23 20:26:02 +0100

    Add files via upload

---
+++ lesson_7/home_work_7.2.py
@@ -0,0 +1,23 @@
+
+# ДЗ 7.2. Модифікувати рядок 
+
+def correct_sentence(text):
+    if not text:
+        return text
+
+
+    text = text[0].upper() + text[1:]
+
+
+    if not text.endswith('.'):
+        text += '.'
+
+    return text
+
+
+assert correct_sentence("greetings, friends") == "Greetings, friends.", "Test1"
+assert correct_sentence("hello") == "Hello.", "Test2"
+assert correct_sentence("Greetings, Friends") == "Greetings, Friends.", "Test3"
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", "Test4"
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", "Test5"
+print("OK")
