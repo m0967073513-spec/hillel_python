adcb381f825b7d8d91d1fd39cc4a04fdb56451a4
Maksicos <maksim.yarovyi@gmail.com>
2026-02-07 16:12:00 +0100

    Update home_work_8.1.py

--- lesson_8/home_work_8.1.py
+++ lesson_8/home_work_8.1.py
@@ -2,12 +2,8 @@
 # ДЗ 8.1. Додати 1 до числа
 
 def add_one(some_list):
-    if some_list == []:
-        return [1, 0]
-
-
     res = some_list[:]
-    carry = 1
+    carry = 1        
 
     for i in range(len(res) - 1, -1, -1):
         s = res[i] + carry
@@ -21,8 +17,23 @@ def add_one(some_list):
 
     return res
 
+
+s = input("Введи число (например 1234): ").strip()
+
+if not s.isdigit():
+    print("Ошибка: нужно ввести только цифры.")
+else:
+    digits = [int(ch) for ch in s]
+    result = add_one(digits)
+
+    print("Список цифр :", digits)
+    print("После +1    :", result)
+    print("Как число   :", "".join(map(str, result)))
+
 assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], "Test1"
 assert add_one([9, 9, 9]) == [1, 0, 0, 0], "Test2"
 assert add_one([0]) == [1], "Test3"
-assert add_one([]) == [1, 0], "Test4"
+assert add_one([9]) == [1, 0], "Test4"
 print("OK")
+
+
